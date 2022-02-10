045157e91dfba2ed8136b697708b3d51f9193c16
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2022-02-09 22:44:33 -0500

    ya todo funcionando en consola

---
+++ main.js
@@ -0,0 +1,6 @@
+const list = document.querySelector('.list');
+
+const sort_name_btn = document.querySelector('.sort-options .sort-name');
+const sort_dmi_btn = document.querySelector('.sort-options .sort-dmi');
+const sort_burguer_btn = document.querySelector('.sort-options .sort-burger');
+
