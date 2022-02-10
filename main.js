9ee707b3c562b252341b8f58938f3c9216a3ab22
Elizabeth Aldana Andrade <70041677+ElizaAldana@users.noreply.github.com>
2022-02-09 22:46:02 -0500

    eliminando cosas

--- main.js
+++
@@ -1,6 +0,0 @@
-const list = document.querySelector('.list');
-
-const sort_name_btn = document.querySelector('.sort-options .sort-name');
-const sort_dmi_btn = document.querySelector('.sort-options .sort-dmi');
-const sort_burguer_btn = document.querySelector('.sort-options .sort-burger');
-
